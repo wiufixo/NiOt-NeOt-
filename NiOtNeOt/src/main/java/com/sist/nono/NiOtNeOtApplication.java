452b0ab0a74c7dd808c218ee59f9daba6c7f6c3d
wiufixo <104910435+wiufixo@users.noreply.github.com>
2022-06-29 21:41:21 +0900

    [first] start settings commit

---
+++ NiOtNeOt/src/main/java/com/sist/nono/NiOtNeOtApplication.java
@@ -0,0 +1,13 @@
+package com.sist.nono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiOtNeOtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiOtNeOtApplication.class, args);
+	}
+
+}
---
+++ NiOtNeOt/src/test/java/com/sist/nono/NiOtNeOtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sist.nono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiOtNeOtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
