61ebc652d4722378b2cf899f63b27c158359edaa
Kim-MinHyeok <48408314+Kim-MinHyeok@users.noreply.github.com>
2022-07-18 00:27:54 +0900

    채팅 이미지 전송 기능 및 기능 개선

--- NiOtNeOt/src/main/java/com/sist/nono/controller/ChatController.java
+++ NiOtNeOt/src/main/java/com/sist/nono/controller/ChatController.java
@@ -1,7 +1,11 @@
 package com.sist.nono.controller;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -11,7 +15,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sist.nono.model.Chat;
@@ -49,5 +55,39 @@ public class ChatController {
 	public String insertChat(int cu_no, int cr_no, String ch_content) {
 		service.insertChat(cu_no, cr_no, ch_content);
 		return "메시지 DB로 송신";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/findRecentChat")
+	public List<Chat> findRecentChat(int cr_no) {
+		List<Chat> chat = service.findRecentChat(cr_no);
+		System.out.println("findRecentChat 컨트롤러에서 보내는 값 : " + chat);
+		return chat;
+	}
+	
+	@ResponseBody
+	@PostMapping("insertChatWithImage")
+	public String insertWithImage(@RequestParam("uploadFile") MultipartFile uploadFile, HttpServletRequest session) {
+		String uploadFolder = "./src/main/resources/static/image/chat";
+		UUID uuid = UUID.randomUUID();
+
+		String uploadFileName = uploadFile.getOriginalFilename();
+		uploadFileName = uuid.toString() + uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+
+		int cu_no = Integer.parseInt(session.getParameter("cu_no"));
+		int cr_no = Integer.parseInt(session.getParameter("cr_no"));
+		String ch_content = (session.getParameter("ch_content"));
+
+		// 파일 다운로드
+		File saveFile = new File(uploadFolder, uploadFileName);
+		try {
+			uploadFile.transferTo(saveFile);
+		} catch (Exception e) {
+			System.out.println("에러:" + e.getMessage());
+		}
+
+		service.insertChatWithImage(cu_no, cr_no, ch_content,uploadFileName);
+
+		return "사진 포함 메시지 DB로 송신";
 	}
 }
--- NiOtNeOt/src/main/resources/static/js/chat.js
+++ NiOtNeOt/src/main/resources/static/js/chat.js
@@ -11,6 +11,7 @@ var btnLogin = getId('btnLogin');
 var btnSend = getId('btnSend');
 var talk = getId('talk');
 var msg = getId('msg');
+var imgSrc = "../image/chat/";
 
 ws = new WebSocket("ws://" + location.host + "/chatt"+"/"+$("#cr_no").val());	// 양방향 통신용 웹소켓 생성
 
@@ -20,6 +21,7 @@ $(function() {
 	var cr_no_json = {
 		"cr_no": crn
 	};
+	
 	$.ajax({
 		url: "/refreshChat",
 		data: cr_no_json,
@@ -31,36 +33,84 @@ $(function() {
 				} else {
 					css = 'class=other';
 				}
-
-				var item = `<div ${css} >
-		                <span><b>${data[index].customer.cu_id}</b></span> [ ${data[index].ch_created} ]<br/>
-                     	<span>${data[index].ch_content}</span>
-						</div>`;
-
+				if(data[index].ch_img == null) {
+					
+					var item = `<div ${css} >
+			                <span><b>${data[index].customer.cu_id}</b></span> [ ${data[index].ch_created} ]<br/>
+	                     	<span>${data[index].ch_content}</span>
+							</div>`;
+				} else {
+					var item = `<div ${css} >
+			                <span><b>${data[index].customer.cu_id}</b></span> [ ${data[index].ch_created} ]<br/>
+	                     	<img id="chatImg" src=${imgSrc}${data[index].ch_img}><br/>
+	                     	<span>${data[index].ch_content}</span>
+							</div>`;
+				}
+				
 				talk.innerHTML += item;
-				talk.scrollTop = talk.scrollHeight;//스크롤바 하단으로 이동
 			});
-		}
+		}, async: false
 	});
+	setTimeout(function() {
+		scroll();
+	}, 50);
 })
 
+function scroll() {
+	console.log(talk.scrollTop);
+	console.log(talk.scrollHeight);
+	talk.scrollTop = talk.scrollHeight;//스크롤바 하단으로 이동
+}
 
 ws.onmessage = function(msg) {
 	var data = JSON.parse(msg.data);
 	var css;
+	var data2;
+	console.log(data)
+	
 	if (data.cu_id == cu_id.value) { // 자신 아이디인지 상대 아이디인지에 따라 적용되는 CSS를 달리 함
 		css = 'class=me';
 	} else {
 		css = 'class=other';
 	}
-
-	var item = `<div ${css} >
-			                <span><b>${data.cu_id}</b></span> [ ${data.date} ]<br/>
-	                      <span>${data.msg}</span>
-							</div>`;
-
+	
+	if (data.type == 0) {
+		var item = `<div ${css} >
+				    	<span><b>${data.cu_id}</b></span> [ ${data.date} ]<br/>
+		                <span>${data.msg}</span>
+					</div>`;
+	} else {
+		var crn = $("#cr_no").val();
+		var item ="";
+		
+		var data = {
+			"cr_no": crn
+		};
+		
+		$.ajax({            
+			url: "/findRecentChat",
+			data: data,
+			dataType: 'json',
+			success: function(message) {
+				data2 = message;
+				console.log("data2 : ");
+				console.log(data2);
+			}, async: false
+		}) 
+		setTimeout(function() {
+		}, 100);
+		item = `<div ${css} >
+			<span><b>${data2[0].customer.cu_id}</b></span> [ ${data2[0].ch_created} ]<br/>
+			<img id="chatImg" src=${imgSrc}${data2[0].ch_img}><br/>
+			<span>${data2[0].ch_content}</span>
+		</div>`;
+	}
+	
 	talk.innerHTML += item;
-	talk.scrollTop = talk.scrollHeight; //스크롤바 하단으로 이동
+	
+	setTimeout(function() {
+		scroll();
+	}, 300);
 }
 
 msg.onkeyup = function(ev) {	// 엔터 키로 전송 가능
@@ -74,16 +124,16 @@ btnSend.onclick = function() {	// 전송 버튼으로 전송 가능
 }
 
 function insert() {
-	console.log("send 버튼 작동");
 	var cuno = $("#cu_no").val();
 	var crn = $("#cr_no").val();
 	var ms = $("#msg").val();
-
+	
 	var data = {
 		"cu_no": cuno
 		, "cr_no": crn
 		, "ch_content": ms
 	};
+	
 	$.ajax({            
 		type : "POST",
 		url: "/insertChat",
@@ -91,20 +141,61 @@ function insert() {
 		success: function(message) {
 			console.log(message);
 		}
+	}) 
+}
+
+function insertWithImage() {
+	var cuno = $("#cu_no").val();
+	var crn = $("#cr_no").val();
+	var ms = $("#msg").val();
+	
+	var formData=new FormData();
+	var inputFile=$("#image_message");
+	var files=inputFile[0].files;
+	
+	formData.append("uploadFile",files[0]);
+	formData.append("cu_no",cuno);
+	formData.append("cr_no",crn);
+	formData.append("ch_content",ms);
+	console.log(formData);
+	
+	$.ajax({
+			url:"/insertChatWithImage",
+			processData:false,
+			contentType:false,
+			data:formData,
+			type:"post",
+			success:function(){
+				console.log("이미지 포함 채팅 업로드 완료");
+			}, async: false
 	})
 }
 
 function send() {
-	insert();
-
-	if (msg.value.trim() != '') {
+	var sendimg=$("#image_message").val();
+	
+	if ($("#msg").val() == "" && sendimg == "") {			// 메시지와 사진 모두 비어있음
+	} else if ($("#msg").val() != "" && sendimg == "") {	// 메시지만 있고 사진은 없음
+		insert();
 		data.cu_id = getId('cu_id').value;
 		data.msg = msg.value;
 		data.date = new Date().toLocaleString();
+		data.type = 0;
+		var temp = JSON.stringify(data);
+		console.log(temp);
+		ws.send(temp);
+		
+	} else  {
+		insertWithImage();
+		data.cu_id = getId('cu_id').value;
+		data.msg = msg.value;
+		data.date = new Date().toLocaleString();
+		data.type = 1;
 		var temp = JSON.stringify(data);
 		console.log(temp);
 		ws.send(temp);
 	}
-
+	
+	$("#image_message").val(""); // 파일 전송창 지우기
 	msg.value = ''; // 메시지 텍스트창 비우기
 }
