a6838847d5cf32aa651093e6536e3f4c9f4defcb
Kim-MinHyeok <48408314+Kim-MinHyeok@users.noreply.github.com>
2022-07-22 11:02:33 +0900

    알람 아이콘 및 기능 연동

--- NiOtNeOt/src/main/java/com/sist/nono/controller/AlertController.java
+++ NiOtNeOt/src/main/java/com/sist/nono/controller/AlertController.java
@@ -1,6 +1,7 @@
 package com.sist.nono.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import com.sist.nono.model.Alert;
 import com.sist.nono.service.AlertService;
+import com.sist.nono.service.CustomerService;
 
 import lombok.Setter;
 
@@ -19,9 +21,12 @@ public class AlertController {
 	@Autowired
 	private AlertService service;
 	
+	@Autowired
+	private CustomerService service2;
+	
 	@GetMapping("/alertList")
-	public String listAlert(Model model) {
-		model.addAttribute("alertlist", service.findAllByCu_no(1));
+	public String listAlert(Model model, Authentication auth) {
+		model.addAttribute("alertlist", service.findAllByCu_no(service2.findByCu_id(auth.getName()).getCu_no()));
 		return "alert/alertList.html";
 	}
 	
